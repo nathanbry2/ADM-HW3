ca900cb3bb5167ab96481fb07bf1ebe25ba30995
nathanbry2 <56337403+nathanbry2@users.noreply.github.com>
2019-11-17 20:49:01 +0100

    Add files via upload
    Python file that once executed generates the indexes of the Search engines

---
+++ index.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import import_ipynb
+from utils import preprocess
+from index_utils import preprocessing_intro_plot, create_index, create_doc_count_words, create_inverted_index
+
+
+# In[ ]:
+
+
+preprocessing_intro_plot()
+
+
+# In[ ]:
+
+
+create_index()
+
+
+# In[ ]:
+
+
+create_doc_count_words()
+
+
+# In[ ]:
+
+
+create_inverted_index()
+
