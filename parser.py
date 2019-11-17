da4b70738d943fa06adaec4606de170723a84e95
nathanbry2 <56337403+nathanbry2@users.noreply.github.com>
2019-11-17 20:22:05 +0100

    Add files via upload
    Python file containing the lines of code needed to parse the entire collection of HTML pages and save those in tsv files

---
+++ parser.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import import_ipynb
+from parser_utils import split_names, parsing_tool, tsv_writer
+
+
+# In[ ]:
+
+
+parsing_tool()
+
+
+# In[ ]:
+
+
+tsv_writer()
+
+
+# In[ ]:
+
+
+
+
